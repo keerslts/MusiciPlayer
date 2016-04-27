a2f540b02650bc21152952a8fd5be3839d485e31
keerslts <1530161972@qq.com>
2016-04-27 12:50:30 +0800

    0426  上下一首歌曲实现

--- app/src/main/java/com/musicplayer/kevin/base/impl/LocalMusicPager.java
+++ app/src/main/java/com/musicplayer/kevin/base/impl/LocalMusicPager.java
@@ -24,6 +24,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.musicplayer.kevin.base.BasePager;
 import com.musicplayer.kevin.inter.ControllerInterface;
@@ -50,7 +51,11 @@ public class LocalMusicPager extends BasePager {
     private Button play;
     private Button next_music;
     private Button up_music;
-    private int is_run ;
+    private int is_run;
+    private String next_path;
+    private String up_path;
+    private int music_number = 0;
+    private int music_position;
 
 
     private static SeekBar sk_bar;
@@ -61,12 +66,12 @@ public class LocalMusicPager extends BasePager {
         return path;
     }
 
-    public static Handler handler = new Handler(){
-        public void handleMessage(Message message){
+    public static Handler handler = new Handler() {
+        public void handleMessage(Message message) {
             Bundle bundle = message.getData();
             int duration = bundle.getInt("duration");
             int currentPosition = bundle.getInt("currentPosition");
-            Log.i(GlobalContents.TAG, "duration: "+duration+"   "+ currentPosition);
+            Log.i(GlobalContents.TAG, "duration: " + duration + "   " + currentPosition);
             sk_bar.setMax(duration);
             sk_bar.setProgress(currentPosition);
         }
@@ -79,6 +84,9 @@ public class LocalMusicPager extends BasePager {
 
     @Override
     public void initData() {
+
+        flContent.removeAllViews();
+
         tvTitle.setText("本地音乐");
         setSlidingMenuEnable(true);
 
@@ -86,38 +94,64 @@ public class LocalMusicPager extends BasePager {
         musics = new ArrayList<>();
         initView();
 
-        View view = View.inflate(mActivity, R.layout.music_content,null);
+        View view = View.inflate(mActivity, R.layout.music_content, null);
         play = (Button) view.findViewById(R.id.play);
         next_music = (Button) view.findViewById(R.id.next_music);
         up_music = (Button) view.findViewById(R.id.up_music);
-
 
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 is_run = new MusicService().getFlag_run();
-                if (is_run==0){
+                if (is_run == 0) {
                     ci.play();
                     play.setText("暂停");
 
-                }else if(is_run%2==0){
+                } else if (is_run % 2 == 0) {
                     ci.continuePlay();
                     play.setText("暂停");
 
-                }else if(is_run%2==1){
+                } else if (is_run % 2 == 1) {
                     ci.pause();
                     play.setText("继续");
-
                 }
 
             }
         });
 
+        next_music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (music_position != music_number - 1) {
+                    next_path = musics.get(music_position + 1).get("filePath").toString();
+                    up_path = null;
+
+                    MusicService.setChange(next_path);
+                    music_position++;
+                }else{
+                    Toast.makeText(mActivity,"当前是最后一首歌", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        up_music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (music_position != 0) {
+                    up_path = musics.get(music_position - 1).get("filePath").toString();
+                    next_music = null;
+                    MusicService.setChange(up_path);
+                    music_position--;
+                }else{
+                    Toast.makeText(mActivity,"当前是第一首歌", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         lv_musics = (ListView) view.findViewById(R.id.lv_music_content);
 
-        lv_musics.setAdapter(new MyAdapter(mActivity,musics));
-//
+        lv_musics.setAdapter(new MyAdapter(mActivity, musics));
 
         flContent.addView(view);
 
@@ -127,6 +161,9 @@ public class LocalMusicPager extends BasePager {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 path = musics.get(position).get("filePath").toString();
+
+                music_position = position;
+
                 MusicService service = new MusicService();
                 service.setChange(path);
                 play.setText("暂停");
@@ -139,7 +176,7 @@ public class LocalMusicPager extends BasePager {
 
     }
 
-    public void startService(View view){
+    public void startService(View view) {
         sk_bar = (SeekBar) view.findViewById(R.id.music_seekBar);
         sk_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -154,13 +191,13 @@ public class LocalMusicPager extends BasePager {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                    //获取sk_bar的当前进度，然后设置给音乐服务的播放进度
+                //获取sk_bar的当前进度，然后设置给音乐服务的播放进度
                 ci.seekTo(seekBar.getProgress());
                 //Log.i(GlobalContents.TAG, "progress: "+ seekBar.getProgress());
             }
         });
 
-        Intent intent = new Intent(mActivity,MusicService.class);
+        Intent intent = new Intent(mActivity, MusicService.class);
 
         intent.putExtra("path", path);
         mActivity.startService(intent);
@@ -181,7 +218,6 @@ public class LocalMusicPager extends BasePager {
     }
 
 
-
     private void initView() {
 
         File file = Environment.getExternalStorageDirectory();
@@ -193,27 +229,25 @@ public class LocalMusicPager extends BasePager {
 
         File[] fs = file.listFiles();
         //file.getAbsolutePath();
-        for(int i=0 ; i<fs.length;i++){
+        for (int i = 0; i < fs.length; i++) {
             File f = fs[i];
-          //  String fileName = f.getName();
+            //  String fileName = f.getName();
             Map<String, Object> map = new HashMap<>();
-            if(f.isDirectory()) {
+            if (f.isDirectory()) {
                 loadMusicFile(f);
-            }else{
-                String flag = f.getName().substring(f.getName().lastIndexOf(".")+1).toLowerCase();
-                if(flag.equals("mp3")){
-                    map.put("fileName",f.getName());
-                    map.put("filePath",f.getAbsolutePath());
-                    map.put("flag", 1);
+            } else {
+                String flag = f.getName().substring(f.getName().lastIndexOf(".") + 1).toLowerCase();
+                if (flag.equals("mp3")) {
+                    map.put("fileName", f.getName());
+                    map.put("filePath", f.getAbsolutePath());
+                    //  map.put("flag", 1);
                     musics.add(map);
-                   // Log.i(GlobalContents.TAG, "music: "+ f.getAbsolutePath());
+                    music_number++;
+                    // Log.i(GlobalContents.TAG, "music: "+ f.getAbsolutePath());
                 }
             }
         }
     }
-
-
-
 
 
     private class MyAdapter extends BaseAdapter {
@@ -243,21 +277,20 @@ public class LocalMusicPager extends BasePager {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            if(convertView==null) {
-                convertView = LayoutInflater.from(ctx).inflate(R.layout.music_list,null);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(ctx).inflate(R.layout.music_list, null);
                 //设置ListView中每一行的宽度和高度
-               // convertView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, Globals.SCREEN_HEIGHT/8));
+                // convertView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, Globals.SCREEN_HEIGHT/8));
             }
 
             TextView musicName = (TextView) convertView.findViewById(R.id.music_name);
             Map<String, Object> map = musics.get(position);
             musicName.setText(map.get("fileName").toString());
 
-           // MainActivity mainActivity = (MainActivity) mActivity;
+            // MainActivity mainActivity = (MainActivity) mActivity;
 
-           // Log.i(GlobalContents.TAG, "filepath: "+ map.get("filePath".toString()));
-           // mainActivity.setPath(map.get("filePath").toString());
-
+            // Log.i(GlobalContents.TAG, "filepath: "+ map.get("filePath".toString()));
+            // mainActivity.setPath(map.get("filePath").toString());
 
 
             return convertView;
